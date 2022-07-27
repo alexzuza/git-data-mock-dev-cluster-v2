afd7353ec04544ae1e365e89ced4ae51ef1ba655
Hung Dang <hd@y42.com>
2022-07-27 16:05:07 +0200

    models: update model "My_SQL_Model"

--- src/Models/SQL/My_SQL_Model/table_1/sql-model.sql
+++ src/Models/SQL/My_SQL_Model/table_1/sql-model.sql
@@ -1,1 +1,1 @@
-SELECT end_time FROM {{ ref('src/Integrations/facebook_pages/page_insight_website_clicks') }} 
+SELECT end_time FROM {{ ref('src/Integrations/facebook_pages/page_insight_website_clicks') }} LIMIT 100
