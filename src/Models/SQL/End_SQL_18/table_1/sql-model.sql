640ebe2881eb5d2deeecff497dcbdfc7c1ee0aeb
Endrit Gjeta <endrit.gjeta@y42.com>
2022-07-29 12:14:16 +0200

    models: update model "End_SQL_18"

--- src/Models/SQL/End_SQL_18/table_1/sql-model.sql
+++ src/Models/SQL/End_SQL_18/table_1/sql-model.sql
@@ -1,15 +1,5 @@
 SELECT 
-Id, 
-Pozitive_numbers, 
-Decimal_numbers, 
-Negative, 
-Negative_double, 
-Text, 
-Id_repeatable, 
-Id_text_repeatable_, 
-Id_of_mix, 
-Boolean, 
-Date, 
-Date_time, 
-Time 
-from {{ ref('src/Integrations/GSH_EE/Copy of Copy of Numeric2-Sheet1') }} where Id>3
+A.id
+from {{ ref('src/Integrations/GSH_EE/Copy of Copy of Numeric2-Sheet1') }} AS A
+JOIN {{ ref('src/Integrations/Google_sheets_all_field_types/Numeric1-Sheet1') }} AS B 
+ON A.Id = B.Id_repeatable
